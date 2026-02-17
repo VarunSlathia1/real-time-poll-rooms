247c7dfe84d7a58b7def9f8b73e4a5999e2ab923
Varun Slathia <varunslathia41@gmail.com>
2026-02-17 23:22:55 +0530

    Update database.js

--- services/database.js
+++ services/database.js
@@ -1,35 +1,72 @@
-const mysql = require('mysql');
+import mysql from 'mysql2';
 
-// Database connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'poll_db'
-});
+class Database {
+  constructor() {
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'your_username',
+      password: process.env.DB_PASSWORD || 'your_password',
+      database: process.env.DB_NAME || 'poll_db'
+    });
+  }
 
-connection.connect(err => {
-  if (err) throw err;
-  console.log('Database connected!');
-});
+  async initialize() {
+    return new Promise((resolve, reject) => {
+      this.connection.connect(err => {
+        if (err) reject(err);
+        console.log('Database connected!');
+        resolve();
+      });
+    });
+  }
 
-// Method to create a new poll
-const createPoll = (pollData) => {
-  const { title, options } = pollData;
-  const query = 'INSERT INTO polls (title, options) VALUES (?, ?)';
-  connection.query(query, [title, JSON.stringify(options)], (err, result) => {
-    if (err) throw err;
-    console.log('Poll created with ID:', result.insertId);
-  });
-};
+  async createPoll(poll) {
+    const query = 'INSERT INTO polls (id, question, options) VALUES (?, ?, ?)';
+    return new Promise((resolve, reject) => {
+      this.connection.query(
+        query,
+        [poll.id, poll.question, JSON.stringify(poll.options)],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  }
 
-// Method to track votes
-const trackVote = (pollId, option) => {
-  const query = 'UPDATE polls SET votes = JSON_ARRAY_APPEND(votes, "$", ?) WHERE id = ?';
-  connection.query(query, [option, pollId], (err, result) => {
-    if (err) throw err;
-    console.log('Vote tracked for poll ID:', pollId);
-  });
-};
+  async getPollById(id) {
+    const query = 'SELECT * FROM polls WHERE id = ?';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [id], (err, results) => {
+        if (err) reject(err);
+        resolve(results[0]);
+      });
+    });
+  }
 
-module.exports = { createPoll, trackVote };
+  async getPollByShareToken(shareToken) {
+    const query = 'SELECT * FROM polls WHERE shareToken = ?';
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, [shareToken], (err, results) => {
+        if (err) reject(err);
+        resolve(results[0]);
+      });
+    });
+  }
+
+  async recordVote(vote) {
+    const query = 'INSERT INTO votes (pollId, optionId, clientIP, deviceFingerprint, timestamp) VALUES (?, ?, ?, ?, ?)';
+    return new Promise((resolve, reject) => {
+      this.connection.query(
+        query,
+        [vote.pollId, vote.optionId, vote.clientIP, vote.deviceFingerprint, vote.timestamp],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        }
+      );
+    });
+  }
+}
+
+export default Database;
